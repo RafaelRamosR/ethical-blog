6bee23803d90e1e4e3ad90ed54e82c7ae9e34c86
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-06 12:20:30 -0500

    Home page with title

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import { Container } from "../components";
+
+export const Home = () => {
+  return (
+    <Container>
+      <h1 className="title">Blog de ética</h1>
+    </Container>
+  )
+};
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,10 @@
 import { Route } from "wouter";
-
-const Page1 = () => <h1>Page 1</h1>
-const Page2 = () => <h1>Page 2</h1>
+import { Home } from "../pages/Home";
 
 function App() {
   return (
     <>
-      <Route path="/users/:name">
-        {(params) => <div>Hello, {params.name}!</div>}
-      </Route>
-      <Route path="/" component={Page1} />
-      <Route path="/page2" component={Page2} />
+      <Route path="/" component={Home} />
     </>
   );
 }
