f3f8998eb98e9d5743ec4cecf635473f4f9594f3
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-06 11:57:04 -0500

    Add wouter

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,17 @@
+import { Route } from "wouter";
+
+const Page1 = () => <h1>Page 1</h1>
+const Page2 = () => <h1>Page 2</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Route path="/users/:name">
+        {(params) => <div>Hello, {params.name}!</div>}
+      </Route>
+      <Route path="/" component={Page1} />
+      <Route path="/page2" component={Page2} />
+    </>
   );
 }
 
