22f70427f51f8ff6a7a242a4154f6b34a6b55257
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-07 14:56:48 -0500

    New route - article

---
+++ src/pages/Article.js
@@ -0,0 +1,16 @@
+import { Link } from 'wouter';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Container } from '../components';
+import { useArticle } from "../hooks/useArticle";
+
+export const Article = ({ params }) => {
+  const markdown = useArticle(params.article);
+
+  return (
+    <Container>
+      <Link href="/">REGRESAR</Link>
+      <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+    </Container>
+  )
+};
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,12 @@
 import { Route } from "wouter";
 import { Home } from "../pages/Home";
+import { Article } from "../pages/Article";
 
 function App() {
   return (
     <>
       <Route path="/" component={Home} />
+      <Route path="/article/:article" component={Article} />
     </>
   );
 }
