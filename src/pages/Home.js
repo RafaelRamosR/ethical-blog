66cbf997fa224bfc44e84e1f499aba3102b23204
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-07 14:21:00 -0500

    Home page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
-import { Container } from "../components";
+import { Container } from '../components';
+import blogData from "../blog/data.json";
 
 export const Home = () => {
   return (
     <Container>
       <h1 className="title">Blog de ética</h1>
+      <main>
+        {
+          blogData.map(blog => {
+            return (
+              <>
+                <h2>{ blog.category }</h2>
+                {
+                  blog.articles.map(article => (
+                    <div>
+                      <h3>{ article.title }</h3>
+                      <p>{ article.subtitle }</p>
+                      <a href={`/article${article.url}`}> Ver más</a>
+                    </div>
+                  ))
+                }
+              </>
+            )
+          })
+        }
+      </main>
     </Container>
   )
 };
