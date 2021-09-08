61f4b23276e738be5ad7ebabc0805b140a7f88f6
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-08 18:03:15 -0500

    Base path added

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,13 @@
-import { Route } from "wouter";
+import { Route, Router } from "wouter";
 import { Home } from "../pages/Home";
 import { Article } from "../pages/Article";
 
 function App() {
   return (
-    <>
+    <Router base="/ethical-blog">
       <Route path="/" component={Home} />
       <Route path="/article/:article" component={Article} />
-    </>
+    </Router>
   );
 }
 
