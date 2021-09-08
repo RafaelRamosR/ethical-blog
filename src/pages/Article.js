7d4528f486dfc7dda2a68dd95b7aee1f26a5d187
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-08 11:13:11 -0500

    Initial layout

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -8,7 +8,7 @@ export const Article = ({ params }) => {
   const markdown = useArticle(params.article);
 
   return (
-    <Container>
+    <Container col="md">
       <Link href="/">REGRESAR</Link>
       <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
     </Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,41 @@
 import { Link } from 'wouter';
 import { Container } from '../components';
-import blogData from "../blog/data.json";
+import blogData from '../blog/data.json';
+import fileIcon from '../assets/file.png';
 
 export const Home = () => {
   return (
-    <Container>
-      <h1 className="title">Blog de ética</h1>
-      <main>
+    <Container col="lg">
+      <h1>Blog de ética</h1>
         {
-          blogData.map(blog => {
+          blogData.map((blog, key) => {
             return (
-              <>
-                <h2>{ blog.category }</h2>
+              <section className="category" key={key}>
+                <h2 className="title">{ blog.category }</h2>
+                <div className="cards">
                 {
-                  blog.articles.map(article => (
-                    <div>
-                      <h3>{ article.title }</h3>
-                      <p>{ article.subtitle }</p>
-                      <Link href={`/article${article.url}`}>Ver más</Link>
-                    </div>
+                  blog.articles.map((article, key) => (
+                      <Link 
+                        className="card"
+                        href={`/article${article.url}`}
+                        key={key}
+                        title={ article.title }
+                      >
+                        <div className="card___icon">
+                          <img src={fileIcon} width="30" alt="file icon" />
+                        </div>
+                        <div className="card__content">
+                          <h3 className="card__title">{ article.title }</h3>
+                          <p className="card__subtitle">{ article.subtitle }</p>
+                        </div>
+                      </Link>
                   ))
                 }
-              </>
+                </div>
+              </section>
             )
           })
         }
-      </main>
     </Container>
   )
 };
