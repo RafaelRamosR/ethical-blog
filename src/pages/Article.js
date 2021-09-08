464fe6467b5fb44adbf393d35d1a84aa3e1c8072
RafaelRamosR <rafael.ramosr@hotmail.com>
2021-09-08 16:45:59 -0500

    Back button

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -3,13 +3,14 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Container } from '../components';
 import { useArticle } from "../hooks/useArticle";
+import '../css/articles.css';
 
 export const Article = ({ params }) => {
   const markdown = useArticle(params.article);
 
   return (
     <Container col="md">
-      <Link href="/">REGRESAR</Link>
+      <Link className="btn-back" href="/">REGRESAR</Link>
       <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
     </Container>
   )
